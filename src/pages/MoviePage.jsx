e29451175053a156234fbf1ac2919d652eed625c
João Victor <joaovictor.developer.frontend@gmail.com>
2023-06-30 19:51:57 -0300

    remoção de hooks que não estavam sendo utilizados

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 
 import { useParams } from "react-router-dom";
 import CardFilmHorizontal from "../components/CardFilmHorizontal";
