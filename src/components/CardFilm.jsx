0f612197c122a0c7abe746c6c3dbbe2f6919c13a
João Victor <joaovictor.developer.frontend@gmail.com>
2023-07-04 09:31:38 -0300

    Link para pagina de filmes adicionado ao título do card, spinner adicionado na pagina dos filmes na renderizção condicional

--- src/components/CardFilm.jsx
+++ src/components/CardFilm.jsx
@@ -1,44 +1,49 @@
-import { useContext} from "react";
+import { useContext } from "react";
 import { ContextTeste } from "../context/context";
-import { BsStarFill } from 'react-icons/bs'
+import { BsStarFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import React from 'react'
+import React from "react";
 
 function CardFilm({ id, title, poster, nota, filme }) {
-
   const [contextState, dispatch] = useContext(ContextTeste);
-  
-  
+
   return (
     <div className="col-xl-3 col-lg-4 col-md-6 col-6" key={id}>
-      
       <div className="card ">
-      <Link to={`/moviepage/${id}`}>
+        <Link to={`/moviepage/${id}`}>
           {poster ? (
-              <img
+            <img
               src={`https://image.tmdb.org/t/p/w500${poster}`}
               className="card-img-top"
               alt="..."
-        />
+            />
           ) : (
-              <div className="card-null card-img-top"></div>
-              )}
+            <div className="card-null card-img-top"></div>
+          )}
+        </Link>
+        <div className="card-body d-flex flex-column justify-content-between">
+          <Link className="card-title" to={`/moviepage/${id}`}>
+            <h5>{title}</h5>
           </Link>
-              <div className="card-body d-flex flex-column justify-content-between">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text d-flex align-items-center">
-                  <BsStarFill className="text-warning me-2"/>{nota.toFixed(1)}
-            </p>
-            
-                <button onClick={() => { dispatch({ type: "FAVORITE", payload: filme }) }}
-                  className={`btn ${contextState.id.includes(id) ? 'btn-danger': 'btn-primary'}`}>
-                  {contextState.id.includes(id) ? "Remover" : "Favoritar"}
-                </button>
-              </div>
+          <p className="card-text d-flex align-items-center">
+            <BsStarFill className="text-warning me-2" />
+            {nota.toFixed(1)}
+          </p>
+
+          <button
+            onClick={() => {
+              dispatch({ type: "FAVORITE", payload: filme });
+            }}
+            className={`btn ${
+              contextState.id.includes(id) ? "btn-danger" : "btn-primary"
+            }`}
+          >
+            {contextState.id.includes(id) ? "Remover" : "Favoritar"}
+          </button>
         </div>
-        
-          </div> 
-  )
+      </div>
+    </div>
+  );
 }
 
-export default CardFilm
+export default CardFilm;
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -3,18 +3,16 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CardFilmHorizontal from "../components/CardFilmHorizontal";
 
-
 function MoviePage() {
-  
   const [filme, setFilme] = useState([]);
   const [trailer, setTrailer] = useState([]);
   const [visible, setVisible] = useState(false);
   const params = useParams();
-  console.log(filme)
+  console.log(filme);
 
   const handleVisible = () => {
-    setVisible(!visible)
-  }
+    setVisible(!visible);
+  };
 
   useEffect(() => {
     fetch(
@@ -23,7 +21,11 @@ function MoviePage() {
       .then(async (resp) => {
         const response = await resp.json();
         setFilme(response);
-        setTrailer(response.videos.results.filter(video => video.type.includes("Trailer"))[0].key);
+        setTrailer(
+          response.videos.results.filter((video) =>
+            video.type.includes("Trailer")
+          )[0].key
+        );
       })
       .catch((error) => {
         console.log(error);
@@ -31,33 +33,41 @@ function MoviePage() {
   }, []);
 
   return (
-    <div className="d-flex flex-column align-items-center my-5 container">
-      <CardFilmHorizontal
-        filme={filme}
-        id={filme.id}
-        title={filme.title}
-        poster={filme.poster_path}
-        overview={filme.overview}
-        vote={filme.vote_average}
-        release={filme.release_date}
-        visible={visible}
-        onHandleVisible={handleVisible}
-      />
+    <>
+      {filme.length !== 0 ? (
+        <div className="d-flex flex-column align-items-center my-5 container">
+          <CardFilmHorizontal
+            filme={filme}
+            id={filme.id}
+            title={filme.title}
+            poster={filme.poster_path}
+            overview={filme.overview}
+            vote={filme.vote_average}
+            release={filme.release_date}
+            visible={visible}
+            onHandleVisible={handleVisible}
+          />
 
-      {visible && (
-      
-          <iframe
-          className="mt-4"
-          id="trailer"
-          src={`https://www.youtube.com/embed/${trailer}`}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
-       
+          {visible && (
+            <iframe
+              className="mt-4"
+              id="trailer"
+              src={`https://www.youtube.com/embed/${trailer}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          )}
+        </div>
+      ) : (
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
       )}
-    </div>
+    </>
   );
 }
 
